f5cb2e67c68579efb822d42986074bca00d2531a
Mac OS developer 1 <turing_alan@icloud.com>
2024-08-28 16:16:40 +0100

    Copied iOS plugins from ppclient to emclient and ppimagemarker for them to be able to compile Xcode in unattended mode.

---
+++ emclient/plugins/ios-codesigningallowed.ts
@@ -0,0 +1,51 @@
+const { createRunOncePlugin, withInfoPlist } = require('expo/config-plugins');
+
+// https://forums.developer.apple.com/forums/thread/656616
+
+
+const withInfoPlistCodeSigningAllowed = (config, id) => {
+  return withInfoPlist(config, async(config) => {
+
+    const fs = require('fs');
+
+    // Modifications to the Podfile
+
+    let PodfileOriginalContents = fs.readFileSync('ios/Podfile', {encoding: 'utf8'});
+
+      let PodfileModifiedContents = PodfileOriginalContents.replace(
+        '    installer.target_installation_results.pod_target_installation_results\n\
+      .each do |pod_name, target_installation_result|\n\
+      target_installation_result.resource_bundle_targets.each do |resource_bundle_target|\n\
+        resource_bundle_target.build_configurations.each do |config|\n\
+          config.build_settings[\'CODE_SIGNING_ALLOWED\'] = \'NO\'\n\
+        end\n\
+      end\n\
+    end\n\
+', 
+        '    installer.target_installation_results.pod_target_installation_results\n\
+      .each do |pod_name, target_installation_result|\n\
+      target_installation_result.resource_bundle_targets.each do |resource_bundle_target|\n\
+        resource_bundle_target.build_configurations.each do |config|\n\
+          config.build_settings[\'CODE_SIGNING_ALLOWED\'] = \'YES\'\n\
+        end\n\
+      end\n\
+    end\n\
+'
+      );
+  
+      fs.writeFileSync('ios/Podfile', PodfileModifiedContents, {encoding: 'utf8'});
+  
+      //console.warn('PodfileModifiedContents:');
+      //console.warn(PodfileModifiedContents);
+
+
+    return config;
+  });
+};
+
+module.exports = createRunOncePlugin(
+  withInfoPlistCodeSigningAllowed,
+  'withInfoPlistCodeSigningAllowed',
+  '1.0.0'
+);
+
---
+++ ppimagemarker/plugins/ios-itms90078-pushnotificationentitlementwarning.ts
@@ -0,0 +1,73 @@
+const { createRunOncePlugin, withInfoPlist } = require('expo/config-plugins');
+
+
+
+const withInfoPlistPushNotificationEntitlement = (config, id) => {
+  return withInfoPlist(config, async(config) => {
+
+    const fs = require('fs');
+    
+
+
+
+    // Modifications AppDelegate.mm
+
+    let AppDelegateOriginalContents = fs.readFileSync('ios/ppimagemarker/AppDelegate.mm', {encoding: 'utf8'});
+
+    let AppDelegateModifiedContents = AppDelegateOriginalContents;
+    
+    AppDelegateModifiedContents = AppDelegateModifiedContents.replace('- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken\n\
+{\n\
+  return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];\n\
+}\n\
+', '// *** This has been commented by ios-itms90078-pushnotificationentitlementwarning.ts plugin\n\
+//- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken\n\
+//{\n\
+//  return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];\n\
+//}\n\
+'
+    );
+
+    AppDelegateModifiedContents = AppDelegateModifiedContents.replace('- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error\n\
+{\n\
+  return [super application:application didFailToRegisterForRemoteNotificationsWithError:error];\n\
+}\n\
+', '// *** This has been commented by ios-itms90078-pushnotificationentitlementwarning.ts plugin\n\
+//- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error\n\
+//{\n\
+//  return [super application:application didFailToRegisterForRemoteNotificationsWithError:error];\n\
+//}\n\
+'
+    );
+
+    AppDelegateModifiedContents = AppDelegateModifiedContents.replace('- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler\n\
+{\n\
+  return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];\n\
+}\n\
+', '// *** This has been commented by ios-itms90078-pushnotificationentitlementwarning.ts plugin\n\
+//- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler\n\
+//{\n\
+//  return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];\n\
+//}\n\
+'
+    );
+
+
+    fs.writeFileSync('ios/ppimagemarker/AppDelegate.mm', AppDelegateModifiedContents, {encoding: 'utf8'});
+  
+    //console.warn('AppDelegateModifiedContents:');
+    //console.warn(AppDelegateModifiedContents);
+  
+
+
+
+    return config;
+  });
+};
+
+module.exports = createRunOncePlugin(
+  withInfoPlistPushNotificationEntitlement,
+  'withInfoPlistPushNotificationEntitlement',
+  '1.0.0'
+);
+
