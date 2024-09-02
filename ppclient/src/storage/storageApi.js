1d8e7eff240075552f1176847f0b8e94fca65091
Mac OS developer 1 <turing_alan@icloud.com>
2024-09-02 11:14:43 +0100

    Comment clarification.

--- ppclient/src/storage/storageApi.js
+++ ppclient/src/storage/storageApi.js
@@ -12,7 +12,7 @@ import { LogMe } from '../myGeneralLibrary.jsx';
 /**
  * 
  * @param {Object} params An object containing key and options. Key is a string, and options is an object
- * @returns Promise with the object value
+ * @returns Promise with the object value. It resolves with null if there is no entry for the given key or if the key has been invalidated.
  */
 export async function load (params) {
     LogMe(1, 'StorageApi.js: load: '+params.key);  
@@ -55,44 +55,3 @@ export async function deleteKey (params) {
         return retval;
     }
 }
-
-
-/*
-// See: https://github.com/sunnylqm/react-native-storage
-
-import Storage from 'react-native-storage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const storage = new Storage({
-    // maximum capacity, default 1000 key-ids
-    size: 10000,
-
-    // Use AsyncStorage for RN apps, or window.localStorage for web apps.
-    // If storageBackend is not set, data will be lost after reload.
-    storageBackend: AsyncStorage, // for web: window.localStorage
-
-    // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
-    // can be null, which means never expire.
-    defaultExpires: null,
-
-    // cache data in the memory. default is true.
-    enableCache: true,
-
-    // if data was not found in storage or expired data was found,
-    // the corresponding sync method will be invoked returning
-    // the latest data.
-
-    sync: {
-    // we'll talk about the details later.
-    }
-  
-  
-});
-
-export default storage;
-
-        // Delete key-value pairs from storage
-        //await AsyncStorage.clear();
-        await storage.clearMap();
-        await storage.clearAll();  // Undocumented function but necessary, otherwise old data reappears
-*/
